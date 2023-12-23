2b1415ea11c0a4f3e3f14469e22cc52d7943e9ce
raabin10 <raabinsinghasokkumar@gmail.com>
2023-12-23 21:36:08 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Singnup from './components/Signup';
+import Login from './components/Login'
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<Singnup/>}/>
+        <Route path='/login' element={<Login/>}/>
+
+      </Routes>
     </div>
   );
 }
